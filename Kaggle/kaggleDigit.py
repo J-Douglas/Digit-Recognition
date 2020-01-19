2469114e71dcac46a33ef0fb28bccaf0e7dfb3ac
John Douglas <j8douglas@uwaterloo.ca>
2020-01-19 16:25:30 -0500

    Local to repo

---
+++ Kaggle/kaggleDigit.py
@@ -0,0 +1,65 @@
+import pandas as pd
+import numpy as np
+import keras
+from keras.models import Sequential
+from keras.layers import *
+from keras.utils import to_categorical
+
+train_dataset = pd.read_csv('datasets/train.csv')
+test_dataset = pd.read_csv('datasets/test.csv')
+
+train_dataset.head()
+
+train_images = train_dataset.iloc[:, 1:785]
+train_labels = train_dataset.iloc[:, 0]
+
+test_images = test_dataset.iloc[:, 0:784]
+
+train_images = train_images.as_matrix().reshape(42000, 784)
+
+test_images = test_images.as_matrix().reshape(28000, 784)
+
+train_images = (train_images/255)-0.7
+test_images = (test_images/255)-0.7
+
+# Flattening the images into a 784 dimensional vector
+train_images = train_images.reshape((-1, 784))
+test_images = test_images.reshape((-1,784))
+
+# Building the model
+ANN_model = Sequential()
+ANN_model.add(Dense(784, activation='relu', input_dim=784))
+ANN_model.add(Dense(300, activation='relu'))
+ANN_model.add(Dense(100, activation='relu'))
+ANN_model.add(Dense(100, activation='relu'))
+ANN_model.add(Dense(100, activation='relu'))
+ANN_model.add(Dense(200, activation='relu'))
+ANN_model.add(Dense(10, activation='softmax'))
+
+# Compiling the model
+ANN_model.compile(
+  optimizer='adam', 
+  loss='categorical_crossentropy', 
+  metrics=['accuracy']
+)
+
+# Training the model
+epoch_count = 50
+ANN_model.fit(
+    train_images, 
+    to_categorical(train_labels), 
+    epochs=epoch_count,
+    batch_size=60
+)
+
+# Saving the model
+ANN_model.save_weights('ANN_Model_{}.h5'.format(epoch_count))
+
+test_pred = pd.DataFrame(ANN_model.predict(test_images, batch_size=60))
+test_pred = pd.DataFrame(test_pred.idxmax(axis = 1))
+test_pred.index.name = 'ImageId'
+test_pred = test_pred.rename(columns = {0: 'Label'}).reset_index()
+test_pred['ImageId'] = test_pred['ImageId'] + 1
+
+test_pred.to_csv('mnist_submission.csv', index = False)
+
---
+++ mnist/ANN.py
@@ -0,0 +1,66 @@
+import numpy as np 
+import mnist
+from keras.models import Sequential
+from keras.layers import Dense, Dropout, Flatten, Conv2D, MaxPooling2D
+from keras.utils import to_categorical
+import random
+import matplotlib.pyplot as plt 
+
+# Training and testing images/labels
+train_images = mnist.train_images() 
+train_labels = mnist.train_labels() 
+test_images = mnist.test_images() 
+
+test_labels = mnist.test_labels()   
+
+# Normalizing the images
+train_images = (train_images/255)-0.7
+test_images = (test_images/255)-0.7
+
+# Flattening the images into a 784 dimensional vector
+train_images = train_images.reshape((-1, 784))
+test_images = test_images.reshape((-1,784))
+
+# Building the model
+ANN_model = Sequential()
+ANN_model.add(Dense(784, activation='relu', input_dim=784))
+ANN_model.add(Dense(64, activation='relu'))
+ANN_model.add(Dense(64, activation='relu'))
+ANN_model.add(Dense(10, activation='softmax'))
+
+# Compiling the model
+ANN_model.compile(
+	optimizer='adam', 
+	loss='categorical_crossentropy', 
+	metrics=['accuracy']
+)
+
+# Training the model
+epoch_count = 60
+ANN_model.fit(
+    train_images, 
+    to_categorical(train_labels), 
+    epochs=epoch_count,
+    batch_size=150
+)
+
+# Saving the model
+ANN_model.save_weights('ANN-Models/ANN_Model_{}.h5'.format(epoch_count))
+
+# Predictions vs. testing images
+num_predictions = 5
+random_lower_bound = random.randint(0,test_images.shape[0]-num_predictions)
+random_upper_bound = random_lower_bound + num_predictions
+predictions = ANN_model.predict(test_images[random_lower_bound:random_upper_bound])
+print (np.argmax(predictions,axis =1))
+print(test_labels[random_lower_bound:random_upper_bound])
+
+# Plotting tests
+for i in range(random_lower_bound,random_upper_bound):
+  first_image = test_images[i]
+  first_image = np.array(first_image, dtype='float')
+  pixels = first_image.reshape((28, 28))
+  plt.imshow(pixels, cmap='gray')
+  plt.show()
+  plt.savefig('Sample-Testing-Images/{}.png'.format(test_labels[i]))
+
